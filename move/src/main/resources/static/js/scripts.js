3d66e96b3c598d6d4cdc569cdd04e3be08c5aaf5
HS-hyeokjin <youngurwls@gmail.com>
2024-02-28 00:31:34 +0900

    feat : offcanvas 추가

--- move/src/main/resources/static/js/scripts.js
+++ move/src/main/resources/static/js/scripts.js
@@ -3,6 +3,16 @@ function toggleBoxOfficeSections() {
     boxOfficeSections.style.display = (boxOfficeSections.style.display === "none") ? "block" : "none";
 }
 
+function toggleMovieDetailSections() {
+    var movieDetailSections = document.getElementById("movieDetailSections");
+    movieDetailSections.style.display = (movieDetailSections.style.display === "none") ? "block" : "none";
+}
+
+function togglePersonDetailSections() {
+    var personDetailSections = document.getElementById("personDetailSections");
+    personDetailSections.style.display = (personDetailSections.style.display === "none") ? "block" : "none";
+}
+
 $(document).ready(function () {
     var navbar = $(".navbar");
     var offset = navbar.offset().top;
@@ -17,12 +27,12 @@ $(document).ready(function () {
 });
 
 $(document).ready(function () {
-    $('.datepicker').datepicker({
+    $('#dailyDatePicker').datepicker({
         format: 'yyyy-mm-dd',
         autoclose: true
     });
 
-    $('.datepicker').on('changeDate', function (e) {
+    $('#dailyDatePicker').on('changeDate', function (e) {
         var selectedDateWithHyphen = e.format();
         var selectedDateWithoutHyphen = selectedDateWithHyphen.replace(/-/g, '');
 
@@ -30,44 +40,45 @@ $(document).ready(function () {
         window.location.href = url;
     });
 });
-$(document).ready(function () {
-    $('.scrollable-row').scrollLeft(0);
-    var scrollSpeed = 2;
-
-    function startScrolling() {
-        var $scrollableRow = $('.scrollable-row');
-        $scrollableRow.animate({
-            scrollLeft: $scrollableRow.scrollLeft() + 200
-        }, scrollSpeed, 'linear', function () {
-            startScrolling();
-        });
-    }
-
-    startScrolling();
-});
 
 $(document).ready(function () {
-    var scrollSpeed = 2;
-    var cardWidth = 0.25;
-
-    function startScrolling() {
-        var $cardContainer = $('.card-container');
-        $cardContainer.animate({
-            marginLeft: '-=' + cardWidth + 'px'
-        }, scrollSpeed, 'linear', function () {
-            if ($cardContainer.offset().left <= -($cardContainer.width())) {
-                $cardContainer.css('margin-left', 0);
-            }
-            startScrolling();
-        });
-    }
-
-    startScrolling();
-});
-
-$(document).ready(function () {
-    $('.datepicker').datepicker({
+    $('#weeklyDatePicker').datepicker({
         format: 'yyyy-mm-dd',
         autoclose: true
     });
+
+    $('#weeklyDatePicker').on('changeDate', function (e) {
+        var selectedDateWithHyphen = e.format();
+        var selectedDateWithoutHyphen = selectedDateWithHyphen.replace(/-/g, '');
+
+        var url = "/weekly-box-office?date=" + selectedDateWithoutHyphen;
+        window.location.href = url;
+    });
+});
+
+$(document).ready(function () {
+    // 스크롤 속도 및 카드의 너비 설정
+    var scrollSpeed = 1;
+    var cardWidth = 0.25;
+
+    // 스크롤을 시작하는 함수
+    function startScrolling() {
+        // 카드 컨테이너 선택
+        var $cardContainer = $('.card-container');
+
+        // 카드를 왼쪽으로 이동하는 애니메이션 적용
+        $cardContainer.animate({
+            marginLeft: '-=' + cardWidth + 'px'
+        }, scrollSpeed, 'linear', function () {
+            // 만약 컨테이너가 왼쪽 끝을 벗어났다면 초기 위치로 되돌아감
+            if ($cardContainer.offset().left <= -($cardContainer.width())) {
+                $cardContainer.css('margin-left', 0);
+            }
+            // 애니메이션이 끝난 후에 다시 시작
+            startScrolling();
+        });
+    }
+
+    // 초기에 스크롤 시작
+    startScrolling();
 });
