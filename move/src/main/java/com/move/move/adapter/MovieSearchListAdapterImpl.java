e3a62e1a80783ea6d6cb825b4880e70fd52ac114
HS-hyeokjin <youngurwls@gmail.com>
2024-03-07 17:45:34 +0900

    fix : UTF-8 인코딩 문제 수정

--- move/src/main/java/com/move/move/adapter/MovieSearchListAdapterImpl.java
+++ move/src/main/java/com/move/move/adapter/MovieSearchListAdapterImpl.java
@@ -33,39 +33,31 @@ public class MovieSearchListAdapterImpl implements MovieSearchListAdapter {
 
     @Override
     public MovieSearchListResponseDto getMovieSearchList(MovieSearchListRequestDto movieSearchListRequestDto) {
-        try {
-            String encodedMovieName = URLEncoder.encode(movieSearchListRequestDto.getMovieNm(), "UTF-8");
-            String encodedDirectorName = URLEncoder.encode(movieSearchListRequestDto.getDirectorNm(), "UTF-8");
+        String requestUrl = url +
+                "?key=" + apiKey +
+                "&movieNm=" + movieSearchListRequestDto.getMovieNm() +
+                "&directorNm=" + movieSearchListRequestDto.getDirectorNm() +
+                "&curPage=" + movieSearchListRequestDto.getCurPage();
 
-            UriComponentsBuilder builder = UriComponentsBuilder.fromHttpUrl(url)
-                    .queryParam("key", apiKey)
-                    .queryParam("movieNm", encodedMovieName)
-                    .queryParam("directorNm", encodedDirectorName)
-                    .queryParam("curPage", movieSearchListRequestDto.getCurPage());
+        ResponseEntity<String> responseEntity = restTemplate.exchange(
+                requestUrl,
+                HttpMethod.GET,
+                null,
+                String.class
+        );
 
-            ResponseEntity<String> responseEntity = restTemplate.exchange(
-                    builder.toUriString(),
-                    HttpMethod.GET,
-                    null,
-                    String.class
-            );
-
-            if (responseEntity.getStatusCode() == HttpStatus.OK) {
-                ObjectMapper objectMapper = new ObjectMapper();
-                try {
-                    String responseBody = responseEntity.getBody();
-                    MovieSearchListResponseDto movieSearchListResponseDto = objectMapper.readValue(responseBody, MovieSearchListResponseDto.class);
-                    return movieSearchListResponseDto;
-                } catch (IOException e) {
-                    e.printStackTrace();
-                    throw new ApiRequestException("API 응답 매핑 실패", e);
-                }
-            } else {
-                throw new ApiRequestException("API 요청 실패. 상태 코드: " + responseEntity.getStatusCodeValue());
+        if (responseEntity.getStatusCode() == HttpStatus.OK) {
+            ObjectMapper objectMapper = new ObjectMapper();
+            try {
+                String responseBody = responseEntity.getBody();
+                MovieSearchListResponseDto movieSearchListResponseDto = objectMapper.readValue(responseBody, MovieSearchListResponseDto.class);
+                return movieSearchListResponseDto;
+            } catch (IOException e) {
+                e.printStackTrace();
+                throw new ApiRequestException("API 응답 매핑 실패", e);
             }
-        } catch (UnsupportedEncodingException e) {
-            e.printStackTrace();
-            throw new ApiRequestException("URL 인코딩 실패", e);
+        } else {
+            throw new ApiRequestException("API 요청 실패. 상태 코드: " + responseEntity.getStatusCodeValue());
         }
     }
 }
